4a66f45ada6a5cc8e90c54162e81969ffc1386f4
tristenwallace <tri3sten@gmail.com>
2024-01-14 16:24:54 -0500

    practice: Decision trees

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -19,13 +19,16 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
-### your code goes here ###
+from sklearn import tree
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
 
+clf = clf.fit(features_train, labels_train)
+
+acc = clf.score(features_test, labels_test)
+
+print("accuracy: {}".format(acc))
 #########################################################
 
-
+print(len(features_train[0]))
