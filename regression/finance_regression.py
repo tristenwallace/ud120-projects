567f91240c7ae546a67c6aa59361003c0d91bdc2
tristenwallace <tri3sten@gmail.com>
2024-01-15 14:27:59 -0500

    practice: Linear regression

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -20,6 +20,7 @@ from feature_format import featureFormat, targetFeatureSplit
 dictionary = joblib.load( open("../final_project/final_project_dataset_modified.pkl", "rb") )
 
 
+
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
@@ -30,19 +31,16 @@ target, features = targetFeatureSplit( data )
 from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
-
-
-### Your regression goes here!
-### Please name it reg, so that the plotting code below picks it up and 
-### plots it correctly. Don't forget to change the test_color above from "b" to
-### "r" to differentiate training points from test points.
-
-
-
-
-
+### Bonus vs. Salary regression
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg = reg.fit(feature_train, target_train)
+r = reg.score(feature_test, target_test)
+print("R-squared: {}".format(r))
+print(reg.coef_)
+print(reg.intercept_)
 
 
 
@@ -57,9 +55,6 @@ for feature, target in zip(feature_train, target_train):
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
 plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
-
-
-
 ### draw the regression line, once it's coded
 try:
     plt.plot( feature_test, reg.predict(feature_test) )
@@ -69,3 +64,23 @@ plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
 plt.show()
+
+### Bonus vs. LTI regression
+features_list = ["bonus", "long_term_incentive"]
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True, sort_keys = '../tools/python2_lesson06_keys.pkl')
+target, features = targetFeatureSplit( data )
+
+### training-testing split needed in regression, just like classification
+from sklearn.model_selection import train_test_split
+feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
+train_color = "b"
+test_color = "r"
+
+### Fit Data
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg = reg.fit(feature_train, target_train)
+r = reg.score(feature_test, target_test)
+print("R-squared: {}".format(r))
+print(reg.coef_)
+print(reg.intercept_)
