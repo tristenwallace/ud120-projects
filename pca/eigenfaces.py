68d24046346d07676566a300435eb78a470a5d67
tristenwallace <tri3sten@gmail.com>
2024-01-16 20:55:43 -0500

    practice: PCA dimensionality reduction

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -64,12 +64,14 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 ###############################################################################
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
 # dataset): unsupervised feature extraction / dimensionality reduction
-n_components = 150
+n_components = 500
 
 print("Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0]))
 t0 = time()
 pca = PCA(n_components=n_components, whiten=True).fit(X_train)
 print("done in %0.3fs" % (time() - t0))
+print("Variance due to 1st PC: ", pca.explained_variance_ratio_[0])
+print("Variance due to 2nd PC: ", pca.explained_variance_ratio_[1])
 
 eigenfaces = pca.components_.reshape((n_components, h, w))
 
@@ -106,6 +108,7 @@ print("done in %0.3fs" % (time() - t0))
 
 print(classification_report(y_test, y_pred, target_names=target_names))
 print(confusion_matrix(y_test, y_pred, labels=range(n_classes)))
+print("Score: ", clf.score(X_test_pca, y_test))
 
 
 ###############################################################################
